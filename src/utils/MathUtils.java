162793be7160ba52283e2332bee6e4c51ae5cded
Prachi Jindal <prachibansal135@gmail.com>
2025-04-11 10:19:40 +0200

    Added MathUtils utility class 🧮

---
+++ src/utils/MathUtils.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class MathUtils {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("5 + 3 = " + add(5, 3));
+        System.out.println("5 - 3 = " + subtract(5, 3));
+    }
+
+}
