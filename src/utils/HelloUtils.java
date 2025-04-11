ead9afcc808823e72fad5b26254b1b6cd3a18760
Prachi Jindal <prachibansal135@gmail.com>
2025-04-11 09:54:12 +0200

    reminder for lunch method

--- src/utils/HelloUtils.java
+++ src/utils/HelloUtils.java
@@ -5,4 +5,11 @@ public class HelloUtils {
     public static String greetTimeOfDay(String name, String time) {
         return "Good " + time + ", " + name + "!";
     }
+
+    public static void reminderForLunch(String name, String time) {
+        if(time.equals("12:00")){
+            System.out.println("Lunch time now " + name);
+        }
+        System.out.println("it's not lunch time " + name );
+    }
 }
